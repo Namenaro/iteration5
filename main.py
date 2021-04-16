8ea37bd116d36ec211b2862e6865bb0b66c77ec6
yana <nanenaro@gmail.com>
2021-04-17 00:10:08 +0300

    fgdgd

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ from multikernel import MultiKernelDevice
 def get_triplets(patient, component, lead_name='i'):
     return patient['Leads'][lead_name]['DelineationDoc'][component]
 
+def get_lead_signal(ecg, lead_name):
+    return ecg['Leads'][lead_name]['Signal']
+
 def handle_ecg_to_3_device(ecg_json, device1, device2, device3):
     i_qrs_triplets = get_triplets(ecg_json, "qrs", "i")
+    i_signal = get_lead_signal(ecg_json, "i")
     for triplet in i_qrs_triplets:
         left = triplet[0]
         center = triplet[1]
         right = triplet[2]
-        print (str(left - right)) # mean 40
+        handle_point_by_device(left, i_signal, device1)
+        handle_point_by_device(center, i_signal, device2)
+        handle_point_by_device(right, i_signal, device3)
+
+
+def handle_point_by_device(coord, signal, device):
+    pass
+
 
 if __name__ == "__main__":
     json_data = get_healthy_7()
